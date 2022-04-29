48b5b40ba13e2f87246200e68507f435468671aa
Juancho <Juan.Aressi@hotmail.com>
2022-04-29 07:33:47 -0300

    CRUD Ready

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -2,11 +2,11 @@ import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackStats() {
-	const {feedback} = useContext(FeedbackContext);
+	const { feedback } = useContext(FeedbackContext);
 
     // Calculate ratings average.
     let average = feedback.reduce((accumulator, current) => {
-        return accumulator + current.rating;
+        return accumulator + +current.rating;
     }, 0) / feedback.length;
 
     average = average.toFixed(1).replace(/[.,]0$/, '');
